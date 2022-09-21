4d24ab5f30932e8bb85fe65aea2d225152ee6f96
sakthiRathinam <sakthiratnam050@gmail.com>
2022-09-21 18:03:46 +0530

    initial commit

---
+++ oops/python/class_prac.py
@@ -0,0 +1,58 @@
+class Os:
+    def privacy(self):
+        print("imhere")
+
+class Test:
+    def test_func(self):
+        print("original func")
+class Testt:
+    def test_funcc(self):
+        print("test func")
+
+
+class Dog(Os, Test):
+    animal = "Dog"
+
+    def __init__(self):
+        self.animal = "scream"
+
+    def test_func(self):
+        super(Dog, self).test_func()
+        print("serewr")
+        
+print(Dog().test_func(), "here")
+print(Dog.animal,"class variable")
+
+
+
+
+
+class Parent(object):
+    def __init__(self):
+        super(Parent, self).__init__()
+        print("parent")
+class Ch(object):
+    def __init__(self):
+        super(Ch, self).__init__()
+        print("Ch")
+
+
+class Left(Ch):
+    def __init__(self):
+        super(Left, self).__init__()
+
+        print("left")
+
+
+class Right(Parent, Ch):
+    def __init__(self):
+        super(Right, self).__init__()
+        print("right")
+
+class Child(Left, Right):
+    def __init__(self):
+        super(Child, self).__init__()
+        print("child")
+
+c = Child()
+print(Child.__mro__)
